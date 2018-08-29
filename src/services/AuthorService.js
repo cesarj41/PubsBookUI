610e2dc2d8dfa8d250a276eeb8123d7714fc0d01
Cesar <cesarj4114@gmail.com>
2018-08-29 11:20:11 -0400

    fixed typo

--- src/services/AuthorService.js
+++ src/services/AuthorService.js
@@ -4,14 +4,14 @@ const url = '/api/authors';
 
 const GetAuthorsTopBooks = () => new Promise(
     (resolve, reject) =>
-        axios.get(url)
+        axios.get(`${url}/books`)
             .then(res => resolve(res.data))
             .catch(err => reject(err)) 
 );
 
 const GetAuthorBooksByName = name => new Promise(
     (resolve, reject) => 
-        axios.get(`${url}/${name}`)
+        axios.get(`${url}/${name}/books`)
             .then(res => resolve(res.data))
             .catch(err => reject(err))
 );
